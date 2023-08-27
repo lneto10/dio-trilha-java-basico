0cba56fb331936f48db4436a9422879328a91bd5
Luiz Neto <105994182+lneto10@users.noreply.github.com>
2023-08-27 17:41:21 -0300

    testes estudos tipos-variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        int numero = 1;
+
+        numero = 2; 
+
+        final double VALOR_DE_PI = 3.14;
+
+        VALOR_DE_PI = 10.75;
+        System.out.println(VALOR_DE_PI);
     }
 }
